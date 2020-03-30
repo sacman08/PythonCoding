be6f08d839423f8dcad59f94c7cff3c28bd9ce33
A. Sacman <sacmail08@gmail.com>
2020-03-30 18:57:23 -0400

    ImageTKViewer updates

--- ImageTkViewer.py
+++ ImageTkViewer.py
@@ -1,19 +1,29 @@
 import sys
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk,Image
 
 root = Tk()
-root.title("TK Fast Image Viewer")
+root.title("Fast Image Viewer")
 
-#TODO Make the browse button work and the image list dynamic to pick photos in a directory.
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=5)
+#frame = ttk.Frame(root)
+#frame['padding'] = (100,10,10,10)
+sizecodes = {'Zoom', '200','175%', '150%', '125%', '100%', '75%', '50%', '25%'}
+sizingvar = StringVar()
+#TODO if the image is larger than the actual screen size, resize border view to make it fit.
+#TODO if image is larger, measure the size and then add sliders to move pic around.
+#TODO add zoom in and out function to resize the image temporarily for viewing.
+
 myimg = Image.open("IMG_5556.jpg")
-myimg= myimg.resize((250,250), Image.ANTIALIAS)
+myimg= myimg.resize((560,480), Image.ANTIALIAS)
 myimg = ImageTk.PhotoImage(myimg)
 imgList=[myimg]
 
 
 my_label= Label(image = imgList)
-my_label.grid(row=0, column=0, columnspan=3)
+my_label.grid(row=0, column=0, columnspan=5)
 
 def forward(image_number):
     global my_label
@@ -28,7 +38,7 @@ def forward(image_number):
     if image_number == max(image_number):
         button_forward = Button(root, text=">>", state=DISABLED)
                          
-    my_label.grid(row=0, column=0, columnspan=3)
+    my_label.grid(row=0, column=0, columnspan=5)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column= 3)
                          
@@ -45,23 +55,27 @@ def back(image_number):
     if image_number == max(image_number):
         button_back = Button(root, text="<<", state=DISABLED)
                          
-    my_label.grid(row=0, column=0, columnspan=3)
+    my_label.grid(row=0, column=0, columnspan=5)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column= 3)
 
-
+#sizingvar = StringVar()
+#sizingvar['values'] = ("Zoom", "200%", "175%", "150%", "125%", "100%", "75%", "50%", "25%")
 button_back = Button(root, text="<<", command=back)
-button_browse = Button(root, text="...")
-button_exit = Button(root, text="Exit", command=root.quit)
 button_forward= Button(root, text=">>", command= lambda: forward(2))
+#button.instate(['!disabled'], cmd)
+button_zoom= ttk.Combobox(root, text=sizecodes{'Zoom'}, variable=sizingvar, value='Zoom')
+button_browse = Button(root, text="Browse...")
+#TODO Make the browse button work and the image list dynamic to pick photos in a directory.
+button_exit = Button(root, text="Exit", command=root.quit)
+
 
 
 button_back.grid(row=1, column=0)
-button_browse.grid(row=1, column = 1)
-button_exit.grid(row=1, column=2)
-button_forward.grid(row=1, column= 3)
+button_forward.grid(row=1, column=1)
+button_zoom.grid(row=1, column=2)
+button_browse.grid(row=1, column=3)
+button_exit.grid(row=1, column=4)
+
 
 root.mainloop()
-#TODO if the image is larger than the actual screen size, resize border view to make it fit.
-#TODO if image is larger, measure the size and then add sliders to move pic around.
-#TODO add zoom in and out function to resize the image temporarily for viewing.
