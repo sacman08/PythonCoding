4e34284bfc4c623389635b33b7ec5f392883db10
A. Sacman <sacmail08@gmail.com>
2020-04-03 10:08:03 -0400

    Adding Factorio.py

---
+++ Factorio.py
@@ -0,0 +1,12 @@
+import collections
+
+Recipes = {"Automation Science Pack":[1,"Iron Gear Wheel", 1, "Copper Plate"],
+           "Iron Gear Wheel":[2, "Iron Plate"],
+           "Iron Plate":[1, "Iron Ore"],
+           "Copper Plate":[1, "Copper Ore"]
+           }
+
+def Calculate_ores(counts, ingredient):
+    global Recipes
+    pass
+
