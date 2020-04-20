da4375d7360a871dd5cbf960784a5a6634c90eee
A. Sacman <sacmail08@gmail.com>
2020-04-20 07:34:17 -0400

    Corrections to file syntaxs

--- ConvertTempsCorF.py
+++ ConvertTempsCorF.py
@@ -9,9 +9,9 @@ def convert2(deg):
     fahr = (deg-32)/1.8
     return '%.1f' % fahr
 
-deg = float(input("Enter the Celsius temperature to convert:"))
-print(convert(deg))
-
-
 fahr = float(input("Enter the Fahrenheit temperature to convert:"))
 print(convert2(fahr))
+
+
+deg = float(input("Enter the Celsius temperature to convert:"))
+print(convert(deg))
--- ConvertingCMandInches.py
+++ ConvertingCMandInches.py
@@ -1,26 +1,21 @@
-from fraction import Fraction
 
-
-def convtoFrac(measure):
-    fract = Fraction(measure)
-    return '%.2f' % fract
 
 def convtoCM(measure):
-    cm = measure * 2.54
+    cm = measure * .39370079
     return cm
 
 def convtoIN(measure):
-    inch = measure * .39370079
+    inch = measure * 2.54
     return inch
 
-units = int(input("What measurement unit use: 1 for cm, 2 for inch"))
+units = int(input("What measurement unit use: 1 for cm, 2 for inch: "))
 if units == 1:
-    length = int(input("Enter the length in cm:"))
-    print(convtoCM(length))
+    length = float(input("Enter the length in cm: "))
+    print('The length in inches is', '%2.f' % convtoCM(length))
                  
 elif units == 2:
-    length = int(input("Enter the length in inches:"))
-    print('The length in inches is', convtoFrac(convtoIN(length)))
+    length = float(input("Enter the length in inches: "))
+    print('The length in cetemeters is', '%2.f' % convtoIN(length))
                  
 else :
     print('Please enter a valid choice')
