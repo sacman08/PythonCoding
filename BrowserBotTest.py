de478f2707908af517a5db966a2b88d74b3e3fec
A. Sacman <sacmail08@gmail.com>
2020-06-19 12:43:09 -0400

    Updates to BrowserBotTest

--- BrowserBotTest.py
+++ BrowserBotTest.py
@@ -3,14 +3,14 @@
 from time import sleep
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+#browser = webdriver.Firefox()
 
 class LoginPage:
     def __init__(self, browser):
         self.browser = browser
 
     def login(self, username, password):
-        username_input = self.browser.find_element_by_css_selector("input[name='username']")
+        username_input = self.browser.find_element_by_css_selector("input[name=\"username\"]")
         password_input = self.browser.find_element_by_css_selector("input[name='password']")
         username_input.send_keys(username)
         password_input.send_keys(password)
