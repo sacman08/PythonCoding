48c5429831cf4f6942e03b5d649d10cfdcb9fd98
A. Sacman <sacmail08@gmail.com>
2020-06-29 08:22:34 -0400

    Fixes to TkImageViewer.py as I learn

--- ImageTkViewer.py
+++ ImageTkViewer.py
@@ -2,13 +2,14 @@ import sys
 import os
 from tkinter import *
 from tkinter import ttk
-from PIL import ImageTk,Image
+from PIL import ImageTk,Image,ImageOps
 from tkinter import filedialog
 
 #TODO if the image is larger than the actual screen size, resize border view to make it fit.
 #TODO add zoom in and out function to resize the image temporarily for viewing.
-#TODO After browse selection, KeyError: 'File name' Unable to getmodebase or getmode on image file
-#TODO May need to use Image.open or a parser to just display file.
+#TODO convert sizevar selection to proper scale (i.e. 200% = 2, 175% = 1.75)
+#TODO After browse selection, Image is showing in window but blank
+#TODO Compile images into a way to select next and previous 
 
 #Build the root image viewing window
 root = Tk()
@@ -23,12 +24,6 @@ content.rowconfigure(0, weight=0)
 #Create the zoom variables for the menu
 sizecodes = ('Zoom','200%','175%','150%','125%','100%','75%','50%','25%')
 sizevar = StringVar()
-#osp = os.path.expanduser(r'~'+os.getlogin())
-# After browsing set the directory and find all image files (Do I even need this loop now?)
-#for file in os.listdir(osp):
-#    if file.endswith(".jpg"):
-#        myimg=(os.path.join(osp, file))
-#        myimg.show()
 imgList = []
 my_label= Label(image = imgList)
 
@@ -62,22 +57,34 @@ def back(image_number):
 
     #if image_number == max(image_number):
     #    button_back = Button(root, text="<<", state=DISABLED)
-                         
+    #TODO Compile images into a way to select next and previous                    
     my_label.grid(row=0, column=0, columnspan=5)
     button_back.grid(row=4, column=0)
     button_forward.grid(row=4, column= 1)
 
 def browse():
     filename = filedialog.askopenfilename(initialdir = r"/", title="Select A File", filetype = (("jpeg", "*.jpg"), ("All Files", "*.*"))) 
-    #imgList = ImageTk.PhotoImage(image=filename)
-    #imgList.show()
-    #imgList = Image.open(filename)  #Image.open uses the OS default image viewer program (i.e. Windows Preview)
-    #imgList.show()
-    #imgList = ImageTk.PhotoImage(myimg)
-    #show first image selected
+    imgList = ImageTk.PhotoImage(Image.open(filename))
+    #TODO If check to reduce size to fit inside monitor size
+    #if imgList.width() > 800 :
+    #    ImageOps.scale(imgList, .5)
+    #    my_label= Label(image = imgList)
+    #    my_label.grid(row=1, column=0)
+    #else:
+    #    my_label= Label(image = imgList)
+    #    my_label.grid(row=1, column=0)       
+    my_label= Label(image = imgList, text=filename)
+    my_label.grid(row=1, column=0, columnspan=5)     
+    #TODO Fix blank image in grid window after "opening" 
+    #TODO Display images to fit in display window (i.e. ImageOps.scale down as needed)
     
 def zoom():
     pass
+    #ImageOps.scale for zooming by size
+    #global sizevar
+    #TODO convert sizevar tuple to dictionary so I can pull resize by selection (i.e sizevar = sizevar_dict.keys) 
+    #sizevar_dict = {'200%' : 2, '175%': 1.75,'150%': 1.5, '125%':1.25,'100%': 1,'75%': .75,'50%': .5,'25%': .25}
+
 
 #Style the grid
 my_label.grid(row=0, column=0, columnspan=5)
