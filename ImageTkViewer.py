b3303a43704baf19b4371424dbba38a766384690
A. Sacman <sacmail08@gmail.com>
2020-06-30 10:56:36 -0400

    Updates

--- ImageTkViewer.py
+++ ImageTkViewer.py
@@ -8,8 +8,8 @@ from tkinter import filedialog
 #TODO if the image is larger than the actual screen size, resize border view to make it fit.
 #TODO add zoom in and out function to resize the image temporarily for viewing.
 #TODO convert sizevar selection to proper scale (i.e. 200% = 2, 175% = 1.75)
-#TODO After browse selection, Image is showing in window but blank
 #TODO Compile images into a way to select next and previous OR remember last images selected to allow for back to and forward to movement.
+#TODO Give it a default image to display on opening with app info and contacts
 
 #Build the root image viewing window
 root = Tk()
@@ -63,19 +63,24 @@ def back(image_number):
     button_forward.grid(row=4, column= 1)
 
 def browse():
+    global my_label
+    global imgList
+    global browse
+    global root
     filename = filedialog.askopenfilename(initialdir = r"/", title="Select A File", filetype = (("jpeg", "*.jpg"), ("All Files", "*.*"))) 
     imgList = ImageTk.PhotoImage(Image.open(filename))
     #TODO If check to reduce size to fit inside monitor size
-    #if imgList.width() > 800 :
-    #    ImageOps.scale(imgList, .5)
-    #    my_label= Label(image = imgList)
-    #    my_label.grid(row=1, column=0)
-    #else:
-    #    my_label= Label(image = imgList)
-    #    my_label.grid(row=1, column=0)       
-    my_label= Label(image = imgList, text=filename)
-    my_label.grid(row=1, column=0, columnspan=5)     
-    #TODO Fix blank image in grid window after "opening" 
+    if imgList.width() > 800 :
+        my_label= Label(image = imgList, text=filename)
+        my_label.image = imgList
+        my_label.grid(row=1, column=0, columnspan=5)
+        #root.geometry("800x600")  resizes main window but crops off buttons and most of image outside width,height
+        #ImageOps.fit(my_label.image, (800,600), method=0, bleed=0, centering=(0.5,0.5))
+    else:
+        my_label= Label(image = imgList, text=filename)
+        my_label.image = imgList
+        my_label.grid(row=1, column=0, columnspan=5)           
+
     #TODO Display images to fit in display window (i.e. ImageOps.scale down as needed)
     
 def zoom():
@@ -88,18 +93,18 @@ def zoom():
 
 #Style the grid
 my_label.grid(row=0, column=0, columnspan=5)
-button_back = Button(root, text="<<", command=back)
-button_forward= Button(root, text=">>", command= lambda: forward(2))
-button_zoom= ttk.Combobox(root, textvariable=sizevar)
-button_zoom['values']=(sizecodes)
-button_zoom.current(0)
+#button_back = Button(root, text="<<", command=back)
+#button_forward= Button(root, text=">>", command= lambda: forward(2))
+#button_zoom= ttk.Combobox(root, textvariable=sizevar)
+#button_zoom['values']=(sizecodes)
+#button_zoom.current(0)
 button_browse = Button(root, text="Browse...", command=browse)
 button_exit = Button(root, text="Exit", command=root.quit)
 
 #Place the buttons
-button_back.grid(row=4, column=0)
-button_forward.grid(row=4, column=1)
-button_zoom.grid(row=4, column=2)
+#button_back.grid(row=4, column=0)
+#button_forward.grid(row=4, column=1)
+#button_zoom.grid(row=4, column=2)
 button_browse.grid(row=4, column=3)
 button_exit.grid(row=4, column=4)
 
