e8e330114660c2d7a60a8626537ee780c26bec21
A. Sacman <sacmail08@gmail.com>
2020-06-30 19:53:18 -0400

    Updates to viewer program

--- ImageTkViewer.py
+++ ImageTkViewer.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python
 import sys
 import os
 from tkinter import *
@@ -5,15 +6,18 @@ from tkinter import ttk
 from PIL import ImageTk,Image,ImageOps
 from tkinter import filedialog
 
-#TODO if the image is larger than the actual screen size, resize border view to make it fit.
+#TODO Give it a default image to display on opening with app info and contacts
+#TODO if the image is larger than the actual screen size, resize view to make it fit.
+#TODO Remove Previous image so that it is not "underneath" current image.
 #TODO add zoom in and out function to resize the image temporarily for viewing.
 #TODO convert sizevar selection to proper scale (i.e. 200% = 2, 175% = 1.75)
-#TODO Compile images into a way to select next and previous OR remember last images selected to allow for back to and forward to movement.
-#TODO Give it a default image to display on opening with app info and contacts
+#(Paused) TODO Compile images into a way to select next and previous OR remember last images selected to allow for back to and forward to movement.
+
 
 #Build the root image viewing window
 root = Tk()
-
+root.wm_title("Fast Image Viewer")
+root.geometry("800x600")
 #Build the content on the frame for navigation
 content = ttk.Frame(root)
 content.grid(column=0, row=0)
@@ -24,8 +28,10 @@ content.rowconfigure(0, weight=0)
 #Create the zoom variables for the menu
 sizecodes = ('Zoom','200%','175%','150%','125%','100%','75%','50%','25%')
 sizevar = StringVar()
+#Create Image variables for opening images
 imgList = []
 my_label= Label(image = imgList)
+
 
 #Functions for menu and picture controls
 def forward(image_number):
@@ -70,18 +76,19 @@ def browse():
     filename = filedialog.askopenfilename(initialdir = r"/", title="Select A File", filetype = (("jpeg", "*.jpg"), ("All Files", "*.*"))) 
     imgList = ImageTk.PhotoImage(Image.open(filename))
     #TODO If check to reduce size to fit inside monitor size
-    if imgList.width() > 800 :
+    if imgList.width() > 800 : #If my image is too big, attempt to scale or fit inside display window (1366x756)
         my_label= Label(image = imgList, text=filename)
         my_label.image = imgList
         my_label.grid(row=1, column=0, columnspan=5)
         #root.geometry("800x600")  resizes main window but crops off buttons and most of image outside width,height
-        #ImageOps.fit(my_label.image, (800,600), method=0, bleed=0, centering=(0.5,0.5))
-    else:
+        #ImageOps.fit(my_label.image, (800,600), method=0, bleed=0, centering=(0.5,0.5)) #ERROR unable to find attiribute size for ImageTK.PhotoImage
+        
+    else:  #Show my image in original size (can be larger than display screen of computer and overlaps (?) browse and exit buttons.
         my_label= Label(image = imgList, text=filename)
         my_label.image = imgList
         my_label.grid(row=1, column=0, columnspan=5)           
 
-    #TODO Display images to fit in display window (i.e. ImageOps.scale down as needed)
+    
     
 def zoom():
     pass
@@ -100,6 +107,8 @@ my_label.grid(row=0, column=0, columnspan=5)
 #button_zoom.current(0)
 button_browse = Button(root, text="Browse...", command=browse)
 button_exit = Button(root, text="Exit", command=root.quit)
+imgList = ImageTk.PhotoImage(Image.open("FastImageViewer_Default.jpg"))
+#TODO Current working directory for default image open?
 
 #Place the buttons
 #button_back.grid(row=4, column=0)
@@ -107,6 +116,7 @@ button_exit = Button(root, text="Exit", command=root.quit)
 #button_zoom.grid(row=4, column=2)
 button_browse.grid(row=4, column=3)
 button_exit.grid(row=4, column=4)
-
+my_label.image = imgList
+my_label.grid(row=1, column=0, columnspan=5)
 
 root.mainloop()
