b538058fae59a7734016060205408bbe419b9092
A. Sacman <sacmail08@gmail.com>
2020-11-15 08:06:03 -0500

    New files added

---
+++ dictionary_tests.py
@@ -0,0 +1,11 @@
+amanda_list = {
+    'first_name': 'Amanda', 
+    'last_name':'Sacco', 
+    'age':'38', 
+    'city':'Atlanta, GA', 
+    'hair_color':'blonde', 
+    'eyes': 'green'
+}
+
+for freaky in amanda_list.values():
+    print(freaky)
