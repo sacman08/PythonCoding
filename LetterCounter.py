0b99e86f821529a4599b0f1398a4f66c2d2613e6
Sacman08 <sacmail08@gmail.com>
2020-11-17 17:52:04 -0500

    upated file

---
+++ LetterCounter.py
@@ -0,0 +1,12 @@
+#Program to counter letters in msg
+
+welcome = 'Welcome to the Letter Counting program!'
+
+print(welcome)
+
+msg = input('Please enter a message:')
+msg_letter = input('Please enter the letter to be counted.')
+
+msg_count = msg.count(msg_letter)
+msg_perc = round((msg_count / len(msg))*100)
+print(f'Your letter, {msg_letter}, was counted {msg_count} times: {msg_perc}%')
