8bf99a5f98a632d0d8f7f5bc24271ad686a9d5b7
sacman08 <sacmail08@gmail.com>
2022-07-10 15:25:34 -0400

    Removing data from Card_Verification

--- 30DaysofPython/Card_verification.py
+++ 30DaysofPython/Card_verification.py
@@ -4,24 +4,24 @@
 # Take the even digits (0,2,4,6, etc.) and double it
 # If any double digit > 9 then subtract 9 from it
 # Add together all the digits
-# If the total results is divisible by 10 means it's valid
+# If the total results is divisible by 10 means it's a valid card number
 
 def luhn_algorithm(card_number):
     card_number = str(card_number)
     card_number = map(int, card_number)
     card_number = list(card_number)  # convert number to a mutable list
-    end_digit = card_number.pop(15)
+    end_digit = card_number.pop(15) # Drop the last digit
     card_number.reverse()  # reverse the numbers
-    for number in range(0, 15, 2):  # take our even digits
+    for number in range(0, 15, 2):  # take our even digits, double them and subtract 9
         card_number[number] = (card_number[number] * 2)
         if card_number[number] > 9:
             card_number[number] = (card_number[number] - 9)
-    card_number.append(end_digit)
-    if sum(card_number) % 10 == 0:
+    card_number.append(end_digit)  # put our end digit back on
+    if sum(card_number) % 10 == 0:  # if the sum of our numbers is dividable by 10 then true, else false.
         check_value = True
     else:
         check_value = False
     return check_value
 
 
-print(luhn_algorithm(5893804115457289))
+print(luhn_algorithm())
