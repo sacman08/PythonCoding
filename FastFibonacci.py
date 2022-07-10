d717cff2116bacca66137683a251dbdf3613d1bc
sacman08 <sacmail08@gmail.com>
2022-07-09 21:41:30 -0400

    Updating files as needed.

--- FastFibonacci.py
+++ FastFibonacci.py
@@ -6,7 +6,7 @@ import sys
 
 
 # returns F(n)
-def fibonacci(n: int):  # noqa: E999 This syntax is Python 3 only
+def fibonacci(n: int):
     if n < 0:
         raise ValueError("Negative arguments are not supported")
     return _fib(n)[0]
@@ -31,15 +31,16 @@ def main(args):
     fibonacci(n)
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    if len(args) != 1:
+    # args = sys.argv[1:]
+    args = input("Find fibonacci to what number:")
+    if len(args) < 1:
         print("Too few or too much parameters given.")
         exit(1)
     try:
-        n = int(args[0])
+        n = int(args)
     except ValueError:
         print("Could not convert data to an integer.")
         exit(1)
-    print("F(%d) = %d" % (n, fibonacci(n)))
+    print("Fibonacci(%d) = %d" % (n, fibonacci(n)))
 
 
--- hn_submissions.py
+++ hn_submissions.py
@@ -1,35 +1,33 @@
 from operator import itemgetter
 import requests
 
-#Make the API call for top stories response file
+# Make the API call for top stories response file
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
 print(f'Status code: {r.status_code}')
 
-#Process file for each story
+# Process file for each story
 submission_ids = r.json()
 submission_dicts = []
 for submission in submission_ids[:30]:
-    #call each submission
+    # call each submission
     url = f'https://hacker-news.firebaseio.com/v0/item/{submission}.json'
     r = requests.get(url)
     print(f'id: {submission}\tstatus: {r.status_code}')
     response_dict = r.json()
 
-    #Build a dict for each sub
-    
+    # Build a dict for each sub
+
     try:
         submission_dict = {
-            'title':response_dict['title'],
+            'title': response_dict['title'],
             'hn_link': f'https://news.ycombinator.com/item?id={submission}',
             'comments': response_dict['descendants']
-            }
+        }
         submission_dicts.append(submission_dict)
-    except:
-        print(f'Problem with one article: {submission}')
+    except KeyError:
+        print(f'Problem with one article: {submission}: {KeyError}')
         pass
-            
-    
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
 
@@ -39,5 +37,3 @@ with open(articles_file, 'w') as f:
         f.write(f"\nTitle: {submission_dict['title']}\n\
                 Discussion: {submission_dict['hn_link']}\n\
                 Comments: {submission_dict['comments']}")
-    
-    
